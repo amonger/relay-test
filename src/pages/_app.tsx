f069dd51c08731296dca202ef327d815321dc2e6
Alan Monger <alan@alanmonger.co.uk>
2023-03-05 20:53:46 +0000

    WIP

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { RelayEnvironmentProvider } from 'react-relay'
+import { initRelayEnvironment} from '../RelayEnvironment'
+
+const environment = initRelayEnvironment()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <RelayEnvironmentProvider environment={environment}>
+    <Component {...pageProps} />
+  </RelayEnvironmentProvider>
 }
